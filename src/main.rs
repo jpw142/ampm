a61cf9c2138feda95d7993e153062e81c348510f
Jack Weber <jackweber142@gmail.com>
2023-09-06 20:21:12 -0400

    worked on asynchronous multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![allow(non_upper_case_globals, non_snake_case, dead_code)]
 use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, math::{Vec3A, Mat3A}};
 use rayon::prelude::*;
-use hashbrown::HashMap;
-use world::chunk_width;
+use world::{Chunk, Node};
 // RwLock is about 10% or 20% slower than mutex
 // However it would allow you to loop through the list of loaded chunks 8 times instead of 27
 // Making 1 of both should be trivial and would just take a few changes so I should test it on
@@ -13,7 +12,6 @@ use world::chunk_width;
 mod cam;
 mod particle;
 mod world;
-mod morton;
 
 use crate::world::World;
 
@@ -42,7 +40,133 @@ fn main() {
                 ))
         .insert_resource(World::new())
         .insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
+        .add_systems(Update, (clear_grid,p2g1).chain())
         .run();
 }
 
+fn initialize (
+    world: ResMut<World>
+) {
+    world.chunks.par_iter().for_each(|(_, c)| {
+        let mut chunk = c.lock().unwrap();
+        for x in 0..Chunk::width {
+            for y in 0..Chunk::width {
+                for z in 0..Chunk::width {
+                    chunk.particles.push(
+                        particle::Particle { 
+                            x: Vec3A::new(x as f32, y as f32, z as f32), 
+                            v: Vec3A::ZERO, 
+                            C: Mat3A::ZERO, 
+                            m: 1.,
+                        }  
+                    );
+                }
+            }
+        }
+    });
+}
 
+fn clear_grid(
+    world: ResMut<World>
+) {
+    world.chunks.par_iter().for_each(|(_, c)| {
+        let chunk = c.lock().unwrap();
+        // If the chunk shouldn't be updated it shouldn't be cleared
+        if !chunk.update {
+            return;
+        }
+        for mut node in chunk.nodes {
+            node.zero(); 
+        }
+    });
+}
+
+fn p2g1 (
+    world: ResMut<World>,
+) {
+    for n in 0..Chunk::loopert_width*Chunk::loopert_width*Chunk::loopert_width {
+        world.chunks.par_iter().for_each(|(&i, c)| {
+            let ch = c.lock().unwrap();
+
+            // If chunk shouldn't be updated or isn't part of the current batch don't do them
+            if !ch.update {
+                return;
+            }
+            if ch.loopert != n {
+                return;
+            }
+            drop(ch);
+
+            let locked_chunks = world.get_surrounding_chunks(i);
+            // Loop through the particles
+            let mut locked_chunk = locked_chunks[Chunk::get_index(3, 1, 1, 1)].lock().unwrap();
+            locked_chunk.particles.clone().iter_mut().for_each(|p| {
+                // Original node coord
+                // All particles must be between 0 and 
+                let ogn_coord = p.x.floor(); 
+                let ogn_diff = (p.x - ogn_coord) - 0.5;
+
+                let weights = [
+                    0.5 * (0.5 - ogn_diff).powf(2.),
+                    0.75 - (ogn_diff).powf(2.),
+                    0.5 * (0.5 + ogn_diff).powf(2.),
+                ];
+                
+
+                for gx in 0..3 {
+                    for gy in 0..3 {
+                        for gz in 0..3 {
+                            let weight = weights[gx].x * weights[gy].y * weights[gz].z;
+
+                            let rn_coord = ogn_coord + Vec3A::new(gx as f32 - 1., gy as f32 - 1., gz as f32 - 1.);  
+                            let rc_coord = Chunk::in_bounds(rn_coord);
+
+                            let Q = p.C * ogn_diff;
+
+                            let m_contrib = weight * p.m;
+
+                            if rc_coord != IVec3::ZERO {
+                                let c_index = Chunk::get_index(3, rc_coord.x + 1, rc_coord.y + 1, rc_coord.z + 1);
+                                // -1 + 1 is 0 so if its in a chunk to the right it will be 0
+                                // -1 + -1 is -2 and -2.rem(9) is 7 which would be the end
+                                let mut outer_chunk_x = rn_coord.x as i32;
+                                let mut outer_chunk_y = rn_coord.y as i32;
+                                let mut outer_chunk_z = rn_coord.z as i32;
+
+                                if rc_coord.x != 0 {outer_chunk_x = (-1 + rc_coord.x).rem_euclid(Chunk::width as i32 + 1);}
+                                if rc_coord.y != 0 {outer_chunk_y = (-1 + rc_coord.y).rem_euclid(Chunk::width as i32 + 1);}
+                                if rc_coord.z != 0 {outer_chunk_z = (-1 + rc_coord.z).rem_euclid(Chunk::width as i32 + 1);}
+
+                                let n_index = Chunk::get_index(Chunk::width, outer_chunk_x, outer_chunk_y, outer_chunk_z);
+                                let mut outside_chunky = locked_chunks[c_index].lock().unwrap();
+                                outside_chunky.nodes[n_index].m += m_contrib;
+                                outside_chunky.nodes[n_index].v += m_contrib * (p.v + Vec3A::from(Q));
+                            }
+                            // If the node is inside the chunk we don't do anything fancy
+                            else {
+                                let n_index = Chunk::get_index(Chunk::width ,rn_coord.x as i32, rn_coord.y as i32, rn_coord.z as i32);
+                                locked_chunk.nodes[n_index].m += m_contrib;
+                                locked_chunk.nodes[n_index].v += m_contrib * (p.v + Vec3A::from(Q));
+                            }
+                        }
+                    }
+                }
+            });
+            drop(locked_chunks);
+        });
+    }
+}
+
+fn p2g2 (
+    world: ResMut<World>,
+) {
+}
+
+
+
+// // Reads all the surrounding chunks and puts them in a list
+// for j in 0..surrounding_chunk_offsets.len(){
+//     let coord = i + surrounding_chunk_offsets[j]; 
+//     let outside_chunk = world.chunks.get(&coord).unwrap().read().unwrap();
+//     locked_chunks.push(outside_chunk);
+// }
--- src/world.rs
+++ src/world.rs
@@ -1,17 +1,7 @@
 use bevy::{prelude::*, math::{Vec3A, Mat3A}};
-use std::sync::RwLock;
+use std::sync::Mutex;
 use crate::particle::Particle;
 use hashbrown::HashMap;
-
-const map_width: usize = 4;
-const map_chunks: usize = map_width * map_width * map_width;
-
-pub const chunk_width: usize = 8;
-const chunk_nodes: usize = chunk_width * chunk_width * chunk_width;
-
-// The length of the buffer 3 for mutex 2 for rwlock
-pub const loopert_width: usize = 2;
-
 
 #[derive(Component, Debug, Clone, Copy,)]
 pub struct Node {
@@ -32,42 +22,107 @@ impl Node {
 }
 
 #[derive(Resource)]
-pub struct World{pub chunks: HashMap<IVec3, RwLock<Chunk>>}
-
+pub struct World{pub chunks: HashMap<IVec3, Mutex<Chunk>>}
 
 pub struct Chunk{
     // lowest bottom left back corner
     pub pos: IVec3,
     pub loopert: usize,
     pub update: bool,
-    pub nodes: [Node; chunk_nodes],
+    pub nodes: [Node; Chunk::num_nodes],
     pub particles: Vec<Particle>,
 }
 
+impl Chunk {
+    pub const loopert_width: usize = 3;
+    pub const width: usize = 8;
+    const num_nodes: usize = Chunk::width * Chunk::width * Chunk::width;
+
+    // If it's in the chunk it will be at 0,0,0 else it gives -1 or +1 depending on surrounding
+    // chunks
+    pub fn in_bounds(n_pos: Vec3A) -> IVec3 {
+        // Relative chunk coord that will tell us if the target node is in the
+        // chunk
+        let mut rc_coord = IVec3::ZERO;
+
+        // Checks if the coordinates is outside the chunk boundaries
+        if n_pos.x > Chunk::width as f32 - 1. {rc_coord.x += 1;}
+        else if n_pos.x < 0. {rc_coord.x -= 1;}
+
+        if n_pos.y > Chunk::width as f32 - 1. {rc_coord.y += 1;}
+        else if n_pos.y < 0. {rc_coord.y -= 1}
+
+        if n_pos.z > Chunk::width as f32 - 1. {rc_coord.z += 1;}
+        else if n_pos.z < 0. {rc_coord.z -= 1}
+        rc_coord
+    }
+
+    pub fn get_index(width: usize, x: i32, y: i32, z:i32) -> usize {
+        let index = (x as usize * width * width) + (y as usize * width) + z as usize;
+        return index
+    }
+}
+
 impl World {
+    const width: usize = 4;
+    const map_chunks: usize = World::width * World::width * World::width;
+    const surrounding_chunk_offsets: [IVec3; 27] = [
+        IVec3::new(-1, -1, -1),
+        IVec3::new(-1, -1, 0),
+        IVec3::new(-1, -1, 1),
+        IVec3::new(-1, 0, -1),
+        IVec3::new(-1, 0, 0),
+        IVec3::new(-1, 0, 1),
+        IVec3::new(-1, 1, -1),
+        IVec3::new(-1, 1, 0),
+        IVec3::new(-1, 1, 1),
+        IVec3::new(0, -1, -1),
+        IVec3::new(0, -1, 0),
+        IVec3::new(0, -1, 1),
+        IVec3::new(0, 0, -1),
+        IVec3::new(0, 0, 0),
+        IVec3::new(0, 0, 1),
+        IVec3::new(0, 1, -1),
+        IVec3::new(0, 1, 0),
+        IVec3::new(0, 1, 1),
+        IVec3::new(1, -1, -1),
+        IVec3::new(1, -1, 0),
+        IVec3::new(1, -1, 1),
+        IVec3::new(1, 0, -1),
+        IVec3::new(1, 0, 0),
+        IVec3::new(1, 0, 1),
+        IVec3::new(1, 1, -1),
+        IVec3::new(1, 1, 0),
+        IVec3::new(1, 1, 1),
+    ];
+
     pub fn new() -> Self {
         let mut world = World{chunks: HashMap::new()};
-        for x in 0..map_width {
-            for y in 0..map_width {
-                for z in 0..map_width {
+        for x in 0..World::width {
+            for y in 0..World::width {
+                for z in 0..World::width {
                     // real modulo, -1.rem_euclid(3) = 2
                     // these are just the x y z mod 3 i decided to name them like this cause i'm
                     // stupid
-                    let looxer = x.rem_euclid(loopert_width);
-                    let looyer = y.rem_euclid(loopert_width);
-                    let loozer = z.rem_euclid(loopert_width); 
-                    let vec: Vec<Node> = (0..chunk_nodes).into_iter().fold(vec![], |mut acc, _| {
+                    let looxer = x.rem_euclid(Chunk::loopert_width);
+                    let looyer = y.rem_euclid(Chunk::loopert_width);
+                    let loozer = z.rem_euclid(Chunk::loopert_width);
+                    let mut edge = false;
+                    if (x % (World::width - 1) == 0) || (y % (World::width - 1) == 0) || (z % (World::width - 1) == 0) {
+                        edge = true;
+                    }
+                    let vec: Vec<Node> = (0..Chunk::num_nodes).into_iter().fold(vec![], |mut acc, _| {
                         acc.push(Node{v: Vec3A::ZERO, m: 0.});
                         acc
                     });
-                    let chunk: [Node; chunk_nodes] = vec.try_into().unwrap();
-                    world.chunks.insert(IVec3::new(x as i32, y as i32, z as i32), RwLock::new(
+                    let chunk: [Node; Chunk::num_nodes] = vec.try_into().unwrap();
+                    world.chunks.insert(IVec3::new(x as i32, y as i32, z as i32), Mutex::new(
                         Chunk{
-                        pos: IVec3::new((x * chunk_width) as i32, (y * chunk_width) as i32, (z * chunk_width) as i32),
+                        pos: IVec3::new((x * Chunk::width) as i32, (y * Chunk::width) as i32, (z * Chunk::width) as i32),
                         nodes: chunk,
-                        update: true,
-                        loopert: (looxer * loopert_width * loopert_width) + (looyer * loopert_width) + loozer,
-                        particles: vec![Particle{x: Vec3A::ZERO, C: Mat3A::ZERO, m: 0., v: Vec3A::ZERO}; chunk_nodes]
+                        update: !edge,
+                        loopert: (looxer * Chunk::loopert_width * Chunk::loopert_width) + (looyer * Chunk::loopert_width) + loozer,
+                        particles: vec![]
                         }
                     ));
                 }
@@ -75,4 +130,87 @@ impl World {
         }
         world
     }
+    pub fn get_surrounding_chunks (&self, pos: IVec3) -> Vec<&Mutex<Chunk>> { 
+            // Gather surrounding chunks
+            let mut surrounding_chunks = vec![];
+            // Reads all the surrounding chunks and puts them in a list
+            for j in 0..World::surrounding_chunk_offsets.len(){
+                let coord = pos + World::surrounding_chunk_offsets[j]; 
+                let outside_chunk = self.chunks.get(&coord).unwrap();
+                surrounding_chunks.push(outside_chunk);
+            }
+            surrounding_chunks
+    }
+}
+#[cfg(test)]
+mod tests {
+    use bevy::math::Vec3A;
+    use crate::world::Chunk;
+    use super::World;
+
+    #[test]
+    fn chunks_update_properly() {
+        let world = World::new();
+        world.chunks.iter().for_each(|(i, c)| {
+            let chunk = c.lock().unwrap();
+            let mut edge = false;
+            if i.x == 0 || i.x == World::width as i32 - 1{
+                edge = true;
+            }
+            if i.y == 0 || i.y == World::width as i32 - 1 {
+                edge = true;
+            }
+            if i.z == 0 || i.z == World::width as i32 - 1 {
+                edge = true;
+            }
+            if edge == true {
+                assert!(chunk.update == false)
+            }
+            else {
+                assert!(chunk.update == true)
+            }
+        });
+    }
+    #[test]
+    fn in_bounds_works() {
+        let mut numbers = vec![];
+        for num in 0..Chunk::width {
+            numbers.push(num);
+        }
+        for x in -1..=Chunk::width as i32 {
+            for y in -1..=Chunk::width as i32 {
+                for z in -1..=Chunk::width as i32 {
+                    let chunk = Chunk::in_bounds(Vec3A::new(x as f32, y as f32, z as f32));
+                    if x == -1 {
+                        assert!(chunk.x == -1);
+                    }
+                    else if x == Chunk::width as i32 {
+                        assert!(chunk.x == 1);
+                    }
+                    else {
+                        assert!(chunk.x == 0);
+                    }
+                    if y == -1 {
+                        assert!(chunk.y == -1);
+                    }
+                    else if y == Chunk::width as i32 {
+                        assert!(chunk.y == 1);
+                    }
+                    else {
+                        assert!(chunk.y == 0);
+                    }
+                    if z == -1 {
+                        assert!(chunk.z == -1);
+                    }
+                    else if z == Chunk::width as i32 {
+                        assert!(chunk.z == 1);
+                    }
+                    else {
+                        assert!(chunk.z == 0);
+                    }
+                }
+            }
+        }
+    }
+    // Write two more tests, 1 for get_surrounding chunks and 1 for making sure loopert works
 }
